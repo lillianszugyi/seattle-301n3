5c235e8a32e2417a3dafb152704b8738e58390db
Megan Walter <megwalter@hotmail.com>
2016-06-26 14:02:02 -0700

    got the searches working and console logging, need to get the city/state pins dropping

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,10 +1,22 @@
-function initMap() {
+function initMap(latty, longy) {
   // Create a map object and specify the DOM element for display.
+  if (!latty || !longy) {
+    latty = 47.611435;
+    longy = -122.330456;
+  }
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.611435, lng: -122.330456},
+    center: {lat: latty, lng: longy},
     scrollwheel: true,
     zoom: 8
   });
 
-  // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+  var marker = new google.maps.Marker ( {
+    position: {lat: latty, lng: longy},
+    map: map,
+  });
 }
+
+
+
+
+  // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -1,7 +1,71 @@
 (function(module) {
 
+  var populateState = function () {
+    webDB.execute(
+      'SELECT DISTINCT state from zips ORDER BY state ASC;',
+      function(results) {
+        results.forEach(function(ele) {
+          var optionTag = '<option value="' + ele.state + '">' + ele.state + '</option>';
+          $('#state-select').append(optionTag);
+        });
+      }
+    );
+  };
+
+  var populateCity = function (state) {
+    webDB.execute(
+      'SELECT DISTINCT city from zips WHERE state=' + '"' + state + '"' + ' ORDER BY city ASC;',
+        function (results) {
+          $('#city-select').empty();
+          $('#city-select').append('<option value="city">Select a City</option>');
+          results.forEach(function(ele) {
+            var optionTag = '<option value="' + ele.city + '">' + ele.city + '</option>';
+            $('#city-select').append(optionTag);
+          });
+        });
+  };
+
+  var zipSearch = function (zip){
+    webDB.execute(
+      'SELECT * from zips WHERE zip=' + zip + ';',
+      function (results) {
+        var lat = results[0].latitude;
+        var long = results[0].longitude;
+        console.log(lat,long);
+        initMap(lat, long)
+      }
+    );
+  };
+  var stateListen = function () {
+    $('#state-select').on('change', function (){
+      populateCity($(this).val());
+      console.log($(this).val());
+    });
+  };
+
+  var cityListen = function() {
+    $('#city-select').on('change', function (){
+      console.log($(this).val());
+    });
+  }
+
+  var zipListen = function () {
+    $('#zipButton').on('click', function (event) {
+      event.preventDefault();
+      var zippy = $("input[name*='zip']").val();
+      if(zippy.length !== 5) {
+        console.log('not a proper zip code');
+      }else{
+        zipSearch(zippy);
+      }
+    });
+  };
+
+
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
-
+  populateState();
+  stateListen();
+  zipListen();
 
 })(window)
