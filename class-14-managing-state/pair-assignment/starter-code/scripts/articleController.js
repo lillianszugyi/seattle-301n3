a4c0df3572fd456b91caf9f33e15751df09e2e56
Lillian Szugyi <lillian.szugyi@gmail.com>
2016-07-10 18:08:43 -0700

    add comments

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -8,6 +8,8 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  //The article variable is passed as a context for a function that is decared as the articleData variable.
+  // The Article.findWhere method will find the id column from the sql table article where the value is matched to the id is passed as ctx and invokes articleData function.
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -18,6 +20,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // Does the same thing as above but finds the authorName and replaces any spaces with + sign and invokes authorData function.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -28,6 +31,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // Does the same thing as above but for categoryName and invokes the categoryData function. This is passed as a variable to be called in the routes.js.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -38,6 +42,8 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // Does the same thing as above for articles, and will not move on to the next function call until all articles are loaded.
+  // Article.fetchAll will grab all the articles from the database if articles does not exist in local storage as rows.
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
